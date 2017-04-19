6c53f5530b877a210758c232233688d82b1ab5bd
Ryan Trad <Ryan Trad>
2017-04-19 17:02:11 -0400

    added bootstrap webpages

--- api.py
+++ api.py
@@ -1,54 +1,145 @@
 import boto3
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, jsonify
+import json
+import uuid
 from config import *
 
 
-s3 = boto3.client('s3', aws_access_key_id = AWS_ACCESS_KEY_ID,
+user_id = "ryantrad"
+table_name = TABLE_NAME_FORMAT#.format(user_id)
+
+
+boto_session = boto3.Session(aws_access_key_id = AWS_ACCESS_KEY_ID,
        aws_secret_access_key = AWS_SECRET_ACCESS_KEY)
+
+s3 = boto_session.client('s3')
+
+dynamodb = boto_session.resource('dynamodb', region_name='us-west-2')
+dynamo_table = dynamodb.Table(table_name)
+
 
 app = Flask(__name__)
 
-
-
-
 @app.route('/api/photo/<photo_id>', methods = ['GET'])
 def get_photo(photo_id):
-    return """<html><h1>Page under construction</h1></html>"""
+    try:
+        response = dynamo_table.get_item(
+                Key={
+                    'photo_id' : photo_id
+                }
+            )
+        key = response['Item']['s3_key']
+        url = s3.generate_presigned_url(
+                ClientMethod='get_object',
+                Params={
+                    'Bucket' : BUCKET_NAME,
+                    'Key' : key
+                }
+            )
+        return jsonify({"url":url})
+    except:
+        return jsonify({"operation_successful" : False})
 
 @app.route('/api/photo', methods = ['POST'])
 def add_photo():
-    print request
-    file = request.files['file']
-    print file
-    if not file.filename == '' and _allowed_file(file.filename):
-        filename = FILE_FORMAT_S3.format(user='null', filename=file.filename)
-        s3.upload_fileobj(file, bucket_name, filename)
-        return redirect('/success')
-    return redirect('/error')
+    if 'file' in request.files:
+        file = request.files['file']
+        
+        if not file.filename == '' and _allowed_file(file.filename):
+            photo_id = str(uuid.uuid4())
+            extension = file.filename.split('.')[-1]
+            
+            filename = FILE_FORMAT_S3.format(user='null', id=photo_id, extension=extension)
+            
+            s3.upload_fileobj(file, BUCKET_NAME, filename)
+            response = dynamo_table.put_item(
+                    Item={
+                        'photo_id' : photo_id,
+                        's3_key' : filename,
+                        'tags' : {
+                            'actions' : [],
+                            'objects' : [],
+                            'people' : [],
+                            'places' : [],
+                            'sentiment' : [],
+                            'time' : []
+                        }
+                    }
+                )
+            
+            return jsonify({"photo_id" : photo_id})
+    return jsonify({"operation_successful" : False})
 
+@app.route('/api/photo/<photo_id>', methods = ['DELETE'])
+def delete_photo(photo_id):
+    try:
+        response = dynamo_table.delete_item(
+                Key={
+                    'photo_id' : photo_id
+                },
+                ReturnValues='ALL_OLD'
+            )
+        key = response['Attributes']['s3_key']
+        response = s3.delete_object(
+                Bucket=BUCKET_NAME,
+                Key=key
+            )
+        success = True
+    except:
+        success = False
+    return jsonify({"operation_successful" : success})
+    
+
+    
+    
+    
 @app.route('/api/photo/<photo_id>/tags', methods = ['GET'])
 def get_tags(photo_id):
-    return """<html><h1>Page under construction</h1></html>"""
+    try:
+        response = dynamo_table.get_item(
+                Key={
+                    'photo_id' : photo_id
+                }
+            )
+        return jsonify({"tags" : response['Item']['tags']})
+    except:
+        return jsonify({"operation_successful" : False})
 
 @app.route('/api/photo/<photo_id>/tags', methods = ['POST'])
-def set_tags(photo_id):
-    return """<html><h1>Page under construction</h1></html>"""
+def post_tags(photo_id):
+    return """<html><h1>API Endpoint under construction</h1></html>"""
 
+@app.route('/api/photo/<photo_id>/tags', methods = ['PUT'])
+def put_tags(photo_id):
+    return """<html><h1>API Endpoint under construction</h1></html>"""
+
+@app.route('/api/photo/<photo_id>/tags', methods = ['DELETE'])
+def delete_tags(photo_id):
+    return """<html><h1>API Endpoint under construction</h1></html>"""
 
 
 @app.route('/')
 def index():
-    return render_template('form.html')
+    try:
+        response = dynamo_table.scan(Limit=3)['Items']
+        for photo in response:
+            key = photo['s3_key']
+            url = s3.generate_presigned_url(
+                ClientMethod='get_object',
+                Params={
+                    'Bucket' : BUCKET_NAME,
+                    'Key' : key
+                }
+            )
+            photo['url'] = url
+        return render_template('home.html', photos = response)
+    except Exception as e:
+        print e
+        return render_template('home.html', photos=[])
 
-
-@app.route('/error')
-def error():
-    return """<html><h1>Encountered an error</h1></html>"""
-
-@app.route('/success')
-def success():
-    return """<html><h1>File upload successful</h1></html>"""
-    
+@app.route('/upload')
+def upload():
+    return render_template('upload_photo.html')
 
 def _allowed_file(filename):
     return '.' in filename and \
---
+++ config.py
@@ -0,0 +1,7 @@
+AWS_ACCESS_KEY_ID = 'YOURKEY'
+AWS_SECRET_ACCESS_KEY = 'YOURSECRETKEY'
+
+BUCKET_NAME = 'photo-schema-app'
+TABLE_NAME_FORMAT = 'photos'
+FILE_FORMAT_S3 = '{user}/{id}.{extension}'
+ALLOWED_EXTENSIONS = ['jpg', 'jpeg', 'png', 'bmp', 'tiff', 'tif', 'gif']
