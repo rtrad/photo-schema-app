3eaf6c8cb8482152d92876167fcb1e646038cfb4
Ryan Trad <trad.ryan.alan@gmail.com>
2017-09-09 18:47:16 -0400

    merged with uploadscreen and uploadpage

--- frontend/src/components/UploadScreen.js
+++ frontend/src/components/UploadScreen.js
@@ -1,57 +1,90 @@
-import React from 'react';
-import $ from 'jquery';
-import {Carousel, Row, Col} from 'react-bootstrap';
+import React, { Component } from 'react';
+import Dropzone from 'react-dropzone';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import FontIcon from 'material-ui/FontIcon';
+import {blue500, red500, greenA200} from 'material-ui/styles/colors';
+import RaisedButton from 'material-ui/RaisedButton';
+import $ from "jquery";
 
-class UploadScreen extends React.Component {
-	constructor(props) {
-		super(props);
-        
-		this.state = {searchquery : '', photos : []};
-		this.fetchPhotos();
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSearch = this.handleSearch.bind(this);
-	}
-    
-    fetchPhotos() {
-		$.ajax({
-			type: "GET",
-			url: 'http://localhost:5000/api/photos/',
-			crossDomain: true,
-			dataType: 'json',
-			success: (result)=>{
-				console.log(result);
-				this.setState({photos : result}); 
-			}
-		});
+
+export default class UploadScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filesPreview:[],
+      filesToBeSent:[],
+      photoCount:100
     }
-	
-	
-	handleChange(event) {
-        const target = event.target;
-        this.setState({[target.name]: target.value}); 
-	}
-	
-    handleSearch(event) {
-		alert('Search is not yet implemented');
+  }
+  onDrop(acceptedFiles, rejectedFiles) {
+  // ('Accepted files: ', acceptedFiles[0].name);
+    console.log(acceptedFiles);
+    var filesToBeSent = this.state.filesToBeSent;
+    if (filesToBeSent.length < this.state.photoCount) {
+      filesToBeSent.push(acceptedFiles);
+      var filesPreview = [];
+      for(var i in filesToBeSent) {
+        filesPreview.push(
+          <div> {filesToBeSent[i][0].name}
+            <MuiThemeProvider>
+              <a href = "#">
+                <FontIcon
+                  className = "material-icons customstyle"
+                  color = {blue500}
+                  styles = {{ top:10,}}
+                  >clear
+                </FontIcon>
+              </a>
+            </MuiThemeProvider>
+          </div>
+        )
+      }
+      this.setState({filesToBeSent,filesPreview});
     }
-    
-    render() {
-		
-		return (
-			<Carousel>
-				{this.state.photos.map(photo =>
-					<Carousel.Item>
-						<Row>
-							<Col sm={3}>
-								<img src={photo.url} />
-							</Col>
-						</Row>
-					</Carousel.Item>
-				
-				)}
-			</Carousel>
-		);
-	}
+    else {
+      alert("You have reached the limit of files you can upload at one time.")
+    }
+    //filesToBeSent.push(acceptedFiles);
+    //this.setState({filesToBeSent});
+  }
+
+  handleClick(event) {
+    // console.log("handleClick",event);
+    var self = this;
+    if (this.state.filesToBeSent.length > 0) {
+      var filesArray = this.state.filesToBeSent;
+      for (var i in filesArray) {
+        console.log("files", filesArray[i]);
+        var fd = new FormData();
+        fd.append("file1",filesArray[i]);
+        $.ajax ({type:"POST", url:"http://localhost:5000/api/photo", data:fd, contentType:"application/octet-stream"});
+      }
+    }
+    else {
+      alert("There are no files to upload.");
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Dropzone onDrop = {(photos) => this.onDrop(photos)}>
+          <div>
+            Drop files here to upload or click in the box to select files to upload.
+          </div>
+        </Dropzone>
+        <div>
+          Photos to be uploaded are:
+          {this.state.filesPreview}
+        </div>
+        <MuiThemeProvider>
+            <RaisedButton
+              label = "Upload"
+              primary = {true}
+              onClick = {(event) => this.handleClick(event)}
+            />
+        </MuiThemeProvider>
+      </div>
+    );
+  }
 }
-
-export default UploadScreen;
