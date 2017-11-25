c99d7d2c91912a085c935955a179d857e35670bf
non <non@lawn-128-61-74-36.lawn.gatech.edu>
2017-11-25 15:58:07 -0500

    update profile page

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -3,30 +3,99 @@ import $ from 'jquery';
 import { Modal, Form, FormGroup, FormControl, ControlLabel, Col, Button } from 'react-bootstrap'; 
 
 const initialState = {
-    username: '',
-    password: '',
-    showRegistration: false,
-    registerUser: {
-        username: '',
-        password: '',
-        confirm_password: '',
-        name : '',
-        email: ''
-    }
+    newemail:'',
+    newpassword: '',
+    confirmnewpassword: '',
+    days: ''
 };
+
 
 class Profile extends React.Component{
     
     constructor(props) {
-    	super(props);
+        super(props);
+        this.state = initialState;
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+
+    
+
     }
 
+    handleChange(event) {
+        const target = event.target;
+        this.setState({[target.name]: target.value});        
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        var formData = {
+            newemail : this.state.newemail,
+            newpassword : this.state.newpassword,
+            confirmnewpassword :this.state.confirmnewpassword,
+            days : this.state.days
+        };
+        console.log('testing');
+        console.log(formData);
+        
+        // $.ajax({
+        //  type: "POST",
+        //  url: 'http://localhost:5000/login',
+        //  crossDomain: true,
+        //  data: formData,
+  //           dataType: 'json',
+        //  success: (result)=>{
+  //               localStorage.setItem('token', result['token']);
+  //               this.context.router.history.push('/home'); 
+  //           },
+  //           error: (result)=>{
+  //               alert('invalid login');
+  //               localStorage.removeItem('token');
+  //               this.setState(initialState);
+  //           }
+        // });
+        }
+
     render(){
-    	return (
-    	 <div>
-    	 	<p>Edit Profile and Adjust Altert </p>
-    	 </div>
-    	)
+        return (
+         <div>
+            <p> Edit Profile and Adjust Altert </p>
+            <Form horizontal onSubmit={this.handleSubmit}>
+                    <FormGroup>
+                        <Col componentClass={ControlLabel} sm={2}>New Email:</Col>
+                        <Col sm={5}>
+                            <FormControl name="newemail" type="text" value={this.state.newemail} onChange={this.handleChange} />
+                        </Col>
+                    </FormGroup> 
+                    <FormGroup>
+                        <Col componentClass={ControlLabel} sm={2}>New Password:</Col>
+                        <Col sm={5}>
+                            <FormControl name="newpassword" type="password" value={this.state.newpassword} onChange={this.handleChange} />
+                        </Col>
+                    </FormGroup>
+                    <FormGroup>
+                        <Col componentClass={ControlLabel} sm={2}>Confirm New Password:</Col>
+                        <Col sm={5}>
+                            <FormControl name="confirmnewpassword" type="password" value={this.state.confirmnewpassword} onChange={this.handleChange} />
+                        </Col>
+                    </FormGroup>
+                    
+                    <FormGroup>
+                        <Col componentClass={ControlLabel} sm={2}>Days untill next notification:</Col>
+                        <Col sm={1}>
+                            <FormControl name="days" type="number" value={this.state.days} onChange={this.handleChange} />
+
+                        </Col>
+                    </FormGroup>
+                    
+                    <FormGroup>
+                        <Col smOffset={2} sm={10}>
+                            <Button type="submit">Save</Button>
+                        </Col>
+                    </FormGroup>
+            </Form>
+         </div>
+        )
     }
 
 }
